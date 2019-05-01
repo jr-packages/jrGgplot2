fc81a5edc3277ce325e222b78c8d24df087c40d3
Rhian Davies <rhian@jumpingrivers.com>
2019-05-01 16:28:33 +0100

    Add lintr test

--- R/overplot_data.R
+++ R/overplot_data.R
@@ -6,10 +6,10 @@
 #' @return Dataframe
 #' @importFrom stats rnorm
 #' @export
-overplot_data = function(n=10000) {
-  x1 = signif(matrix(rnorm(n), ncol=2), 2)
+overplot_data = function(n = 10000) {
+  x1 = signif(matrix(rnorm(n), ncol = 2), 2)
   x2 = signif(matrix(
-    rnorm(n, mean=3, sd=1.5), ncol=2), 2)
+    rnorm(n, mean = 3, sd = 1.5), ncol = 2), 2)
   x = rbind(x1, x2)
-  data.frame(x=x[,1], y=x[,2])
+  data.frame(x = x[, 1], y = x[, 2])
 }
